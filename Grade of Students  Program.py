349a605f76d063c7fa877b5d083871fa8b18f715
Aryan Mujawar <aryanmujawar435@gmail.com>
2026-08-11 23:28:55 +0530

    Add files via upload

---
+++ Grade of Students  Program.py
@@ -0,0 +1,16 @@
+marks = int(input("enter marks of Students:"))
+
+
+if(marks >=90):
+     grade="A"
+
+elif(marks >=90 and marks <=80):
+    grade="B"
+
+elif(marks >=80 and marks <=70):
+     grade="C"
+
+else:
+       grade="D"
+
+print( "grade of students ->",grade)
